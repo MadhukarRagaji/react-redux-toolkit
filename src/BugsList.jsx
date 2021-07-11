a3b9fb5b9a41bb17a60128f7848410ecfa92dd0a
MadhukarRagaji <32383763+MadhukarRagaji@users.noreply.github.com>
2021-07-12 00:54:24 +0530

    react with redux using redux tool kit

---
+++ src/BugsList.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadBugs, getUnresolvedBugs, resolveBug } from "./store/bugs";
+
+const BugsList = () => {
+  const dispatch = useDispatch();
+
+  const bugs = useSelector(getUnresolvedBugs);
+
+  useEffect(() => {
+    dispatch(loadBugs());
+  }, []);
+
+  return (
+    <div>
+      Bugs List
+      <ul>
+        {bugs.map((bug) => (
+          <li key={bug.id}>
+            {bug.description}
+            <button onClick={() => dispatch(resolveBug(bug.id))}>
+              Resolve
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BugsList;
